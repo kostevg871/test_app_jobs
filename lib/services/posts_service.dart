45d56f608498fa29f139295d29525c144df19944
Evgeniy <kostyrkoevgeniy@yandex.ru>
2020-11-25 20:44:10 +0300

    add albumsTile, no foto

---
+++ lib/services/posts_service.dart
@@ -0,0 +1,18 @@
+import 'package:http/http.dart' as http;
+import 'package:json_test_project/models/posts.dart';
+
+class PostService {
+  http.Client client = http.Client();
+
+  Future<List<Posts>> fetchPost(user) async {
+    var userId = user.id;
+    var url = "https://jsonplaceholder.typicode.com/posts?userId=$userId";
+    var response = await client.get(url);
+    if (response.statusCode == 200) {
+      print(response.body);
+      return postsFromJson(response.body);
+    } else {
+      print("err");
+    }
+  }
+}
