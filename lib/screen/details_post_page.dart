a552dfd71346148368422a94a7d0848ccf90bbf9
Evgeniy <kostyrkoevgeniy@yandex.ru>
2020-11-27 01:39:08 +0300

    add bottomsheets addCommentScreen

--- lib/screen/details_post_page.dart
+++ lib/screen/details_post_page.dart
@@ -43,6 +43,98 @@ class DetailsPostPage extends StatelessWidget {
             ],
           ),
         ),
-        floatingActionButton: CustomFlatButton());
+        floatingActionButton: GestureDetector(
+          onTap: () {
+            showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (context) => SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).viewInsets.bottom),
+                  child: AddCommentScreen(),
+                ),
+              ),
+            );
+          },
+          child: CustomFlatButton(),
+        ));
+  }
+}
+
+class AddCommentScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String newCommentTitle;
+    String newCommentEmail;
+    String newCommentBody;
+
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              "Add Comment",
+              style: TextStyle(fontSize: 30.0, color: Colors.indigo),
+              textAlign: TextAlign.center,
+            ),
+            TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                labelText: "Name",
+              ),
+              textAlign: TextAlign.center,
+              onChanged: (newText) {
+                newCommentTitle = newText;
+              },
+            ),
+            TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                labelText: "e-mail",
+              ),
+              textAlign: TextAlign.center,
+              onChanged: (newText) {
+                // newTaskTitle = newText;
+              },
+            ),
+            TextField(
+              maxLines: 2,
+              maxLength: 255,
+              obscureText: false,
+              decoration: InputDecoration(
+                labelText: "Comment",
+              ),
+              textAlign: TextAlign.center,
+              onChanged: (newText) {
+                newCommentTitle = newText;
+              },
+            ),
+            FlatButton(
+              onPressed: () {
+                // Provider.of<TaskData>(context, listen: false)
+                //     .addTask(newTaskTitle);
+                // Navigator.pop(context);
+              },
+              child: Text(
+                "Add",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.indigo,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
